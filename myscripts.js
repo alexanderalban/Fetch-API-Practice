959964070dba48714f9ea0ca3a9507b087f77b12
alexanderalban <albanalexander@gmail.com>
2020-10-06 15:18:21 -0500

    button to activate API and hello message

---
+++ myscripts.js
@@ -0,0 +1,7 @@
+let MagicButton = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(myJson => console.log(myJson))
+    console.log("hello there")
+
+}
