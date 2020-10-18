741aa67fa1872af03f557861a5ce930788df9a56
alexanderalban <albanalexander@gmail.com>
2020-10-17 21:25:09 -0500

    format changes for legibility:

--- myscripts.js
+++ myscripts.js
@@ -2,6 +2,6 @@ let MagicButton = () => {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(response => response.json())
     .then(myJson => console.log(myJson))
-    console.log("hello there")
+    console.log("hello there");
 
 }
